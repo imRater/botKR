ce192aadb57031094906675d87b55ca0c69965e3
imRater <uujduj935@gmail.com>
2026-05-07 18:36:11 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- 123.py
+++ 123.py
@@ -12,7 +12,7 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-DB_URL = "postgresql://postgres:221221poN!_123@aws-0-eu-west-1.pooler.supabase.com:5432/postgres"
+DB_URL = "postgresql://postgres:221221poN!_123@aws-0-eu-west-1.pooler.supabase.com:5432/postgres"options=-c%20search_path%3Dpublic"
 
 # --- КОНФИГУРАЦИЯ ---
 TOKEN = os.getenv("BOT_TOKEN") # ЗАМЕНИ НА НОВЫЙ!
